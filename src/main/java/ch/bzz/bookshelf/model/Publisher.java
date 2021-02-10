9bb09fb5d9883037d9c8423b7a78496f5209d657
DavideDiGiovanni18 <digiovannid@bzz.ch>
2021-02-10 11:39:37 +0100

    add model and service classes

---
+++ src/main/java/ch/bzz/bookshelf/model/Publisher.java
@@ -0,0 +1,22 @@
+package ch.bzz.bookshelf.model;
+
+public class Publisher {
+    private String publisher;
+    private String publisherUUID;
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publsiher) {
+        this.publisher = publsiher;
+    }
+
+    public String getPublisherUUID() {
+        return publisherUUID;
+    }
+
+    public void setPublisherUUID(String publsiherUUID) {
+        this.publisherUUID = publsiherUUID;
+    }
+}
